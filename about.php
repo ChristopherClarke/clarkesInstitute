abeb3be78fd85eb151cb4fc23e84cda239e81a9e
ChristopherClarke <christopher.clarke73@yahoo.com>
2021-12-20 19:14:51 -0500

    About, Admin, Contact pages added

---
+++ about.php
@@ -0,0 +1,41 @@
+<?php 
+    $title='Registration';
+    require_once 'includes/header.php';
+?>
+<div class="header50 jumbotron-fluid text-center" style=" background-image: url('./uploads/about1.jpg');" >
+  <div class=" jumbotext " >
+    <h1 class="display-4 text-white p-2 hd" >About Us</h1>
+  </div>
+</div>
+<div class="row p-5">    
+    <div class="col p-4 " style="background-color:white;" >
+        <h2 class="hd1">Welcome from the Head</h2><br>
+      <p class="info1">
+      Our 133rd year of teaching and learning begins with sustained commitment
+       to our liberal arts foundation, as well as the promise to continually define
+        excellence in education for the world our students will inhabit. 
+        Our vision challenges us to make learning even more personal, 
+        inquiry-based and inclusive for every member of the English community.
+        <br><br>
+        As a result, Latin learners are conscious and proactive about their learning,
+         paving the way for the development of critical skills and an understanding 
+         of who they are that serves them not just in college, but for a lifetime.
+      </p>
+    </div>
+    <div class="col-6">
+     <img src="uploads/register1.jpg" class="img img-fluid">
+    </div>
+  </div>  
+  <div class="row p-2"> <hr>  </div>
+
+    <div class="row p-5"> 
+    <h2 class="hd1">Community Values</h2><br>
+    
+    Our shared values of excellence, community and integrity affirm and define us.
+     They serve as guideposts for all we do, and connect us — students, teachers, 
+     parents and alumni.
+
+    </div>
+
+
+<?php require_once 'includes/footer.php'; ?>
---
+++ contact.php
@@ -0,0 +1,46 @@
+<?php 
+    $title='Registration';
+    require_once 'includes/header.php';
+?>
+<div class="header50 jumbotron-fluid text-center" style=" background-image: url('./uploads/contact1.jpg');" >
+  <div class=" jumbotext">
+    <h1 class="display-4 text-white p-2 hd">Contact Us</h1>
+  </div>
+</div>
+<div class="row p-5"> 
+<h2 class="hd1 text-center">Get In Touch.</h2><br>
+</div>
+<div class="row p-5 text-center">
+    <div class="col-4">
+        <h4>Office of the Head of School</h4>
+        <hr>       
+        <p>Randall Dunn<br> 1876-776-5463</p>
+        <br><br>
+        <h4>Facilities and Operations</h4>
+        <hr>       
+        <p>Jak E. Chan<br> 1876-653-8872</p>
+    </div>
+    <div class="col-4">
+        <h4>Admissions</h4>
+        <hr>        
+        <p>Allan Walker<br>1876-990-9875</p>
+        <br><br>
+        <h4>Development</h4>
+        <hr>       
+        <p>Sindy Rellah<br> 1876-435-8907</p>
+    </div>
+    <div class="col-4">
+        <h4>Registrar</h4>
+        <hr>       
+        <p>Dee Lylah<br>1876-234-1276</p>
+        <br><br>
+        <h4>General inquiries</h4>
+        <hr>       
+        <p>info@clarkesinstitute.org<br> 1876-992-3343</p>
+    </div>
+    
+
+</div>
+
+
+<?php require_once 'includes/footer.php'; ?>
