61e6fe02f245de87c2b67a659006da8e8fecfc34
ChristopherClarke <christopher.clarke73@yahoo.com>
2021-12-20 19:07:33 -0500

    Added Index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php 
+    $title='Index';
+    require_once 'includes/header.php';
+    require_once 'db/conn.php';
+ ?>
+
+
+ <div class="jumbotron jumbotron-fluid text-center" >
+  <div class="container jumbotext " >
+    <h1 class="display-4 text-white p-2 hd" >Clarke`s Institute</h1><br>
+    <p class="lead text-white p-2" >A learning community dedicated to building respectful and responsible citizens and empowering all learners.</p>
+  </div>    
+</div>
+<div class="row text-center wel">
+   <h2>WELCOME TO CLARKE`S INSTITUTE.  ACHIEVING GREATNESS SINCE 1854.</h2>
+   <br><br>
+</div>
+<div class="row text-center" style="background-color:#f3f3f3">   
+   <h3><br>What Makes Clarke`s Institute Unique</h3>
+</div>
+<div class="row p-5" style="background-color:#f3f3f3">
+    <div class="col-7">
+     <img src="uploads/academics2.jpg" class="img">
+    </div>
+    <div class="col text-center p-4 " style="background-color:white;" >
+      <p style="font-size:130%">
+      For more than 130 years we've put students at the heart of everything we do. 
+      We're committed to seeing everyone within these walls succeed and reach their 
+      full potential through an educational experience that will empower them to tinker, 
+      to make mistakes, to ask questions, to change how they see the world around them.
+      </p>
+    </div>
+  </div>
+<div class="row">
+<div class="row text-center wel">
+   <div class="col-9">
+      <h3>INTERESTED IN STUDYING AT CLARKE`S INSTITUTE?</h3>
+   </div>
+   <div class="col">
+      <a href="register.php" class="btn btn-info">Apply Now!</a>
+   </div>  
+</div>
+</div>
+
+<div class="container">
+   
+    
+    <?php require_once 'includes/footer.php'; ?>
